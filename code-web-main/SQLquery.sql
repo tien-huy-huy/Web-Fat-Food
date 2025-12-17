93b1b913f6841e41b652ff7c1759b489f5cd1d7d
Tien Huy <nanithe123456@gmail.com>
2025-12-17 07:21:43 +0700

    sql

---
+++ code-web-main/SQLquery.sql
@@ -0,0 +1,257 @@
+USE [master]
+GO
+/****** Object:  Database [tintuconline]    Script Date: 12/14/2025 12:05:49 PM ******/
+CREATE DATABASE [tintuconline]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'tintuconline', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER02\MSSQL\DATA\tintuconline.mdf' , SIZE = 4096KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
+ LOG ON 
+( NAME = N'tintuconline_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER02\MSSQL\DATA\tintuconline_log.ldf' , SIZE = 3136KB , MAXSIZE = 2048GB , FILEGROWTH = 10%)
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT, LEDGER = OFF
+GO
+ALTER DATABASE [tintuconline] SET COMPATIBILITY_LEVEL = 110
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [tintuconline].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [tintuconline] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [tintuconline] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [tintuconline] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [tintuconline] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [tintuconline] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [tintuconline] SET AUTO_CLOSE OFF 
+GO
+ALTER DATABASE [tintuconline] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [tintuconline] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [tintuconline] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [tintuconline] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [tintuconline] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [tintuconline] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [tintuconline] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [tintuconline] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [tintuconline] SET  DISABLE_BROKER 
+GO
+ALTER DATABASE [tintuconline] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [tintuconline] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [tintuconline] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [tintuconline] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [tintuconline] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [tintuconline] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [tintuconline] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [tintuconline] SET RECOVERY SIMPLE 
+GO
+ALTER DATABASE [tintuconline] SET  MULTI_USER 
+GO
+ALTER DATABASE [tintuconline] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [tintuconline] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [tintuconline] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [tintuconline] SET TARGET_RECOVERY_TIME = 0 SECONDS 
+GO
+ALTER DATABASE [tintuconline] SET DELAYED_DURABILITY = DISABLED 
+GO
+ALTER DATABASE [tintuconline] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+EXEC sys.sp_db_vardecimal_storage_format N'tintuconline', N'ON'
+GO
+ALTER DATABASE [tintuconline] SET QUERY_STORE = OFF
+GO
+USE [tintuconline]
+GO
+/****** Object:  Table [dbo].[Comment]    Script Date: 12/14/2025 12:05:49 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Comment](
+	[ma] [int] IDENTITY(1,1) NOT NULL,
+	[noidung_cm] [nvarchar](100) NOT NULL,
+	[ngay] [datetime] NOT NULL,
+	[ma_taikhoan] [int] NOT NULL,
+	[ma_tin] [int] NOT NULL,
+	[trangthai] [bit] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ma] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[danhmuc]    Script Date: 12/14/2025 12:05:49 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[danhmuc](
+	[dm_ma] [int] IDENTITY(1,1) NOT NULL,
+	[dm_ten] [nvarchar](50) NULL,
+	[dm_anh] [nvarchar](50) NULL,
+	[dm_hot] [bit] NULL,
+	[dm_thutu] [bit] NULL,
+	[dm_trangthai] [bit] NULL,
+	[dm_parent] [int] NULL,
+ CONSTRAINT [PK_danhmuc] PRIMARY KEY CLUSTERED 
+(
+	[dm_ma] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[LEVEL]    Script Date: 12/14/2025 12:05:49 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[LEVEL](
+	[levelID] [int] IDENTITY(1,1) NOT NULL,
+	[levelname] [nvarchar](30) NOT NULL,
+	[levelstatus] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[levelID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[PERMISSION]    Script Date: 12/14/2025 12:05:49 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[PERMISSION](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[levelID] [int] NOT NULL,
+	[permitName] [nvarchar](100) NOT NULL,
+	[ValueName] [nvarchar](100) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[ProdCategory]    Script Date: 12/14/2025 12:05:49 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ProdCategory](
+	[IDCate] [int] IDENTITY(1,1) NOT NULL,
+	[CateName] [nvarchar](100) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[IDCate] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[product]    Script Date: 12/14/2025 12:05:49 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[product](
+	[IDProd] [int] IDENTITY(1,1) NOT NULL,
+	[NameProd] [nvarchar](100) NULL,
+	[Price] [int] NULL,
+	[ImageProd] [nvarchar](255) NULL,
+	[Size] [int] NULL,
+	[Khuyenmai] [bit] NULL,
+	[DescProd] [nvarchar](200) NULL,
+	[TypeProd] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[IDProd] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[taikhoan]    Script Date: 12/14/2025 12:05:49 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[taikhoan](
+	[ma] [int] IDENTITY(1,1) NOT NULL,
+	[tennguoidung] [nvarchar](100) NULL,
+	[matkhau] [varchar](100) NOT NULL,
+	[trangthai] [bit] NULL,
+	[LevelID] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ma] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[tintuc]    Script Date: 12/14/2025 12:05:49 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[tintuc](
+	[tt_ma] [int] IDENTITY(1,1) NOT NULL,
+	[tt_tieude] [text] NULL,
+	[tt_trichdan] [text] NULL,
+	[tt_noidung] [text] NULL,
+	[tt_anh] [text] NULL,
+	[tt_ngay] [date] NULL,
+	[dm_ma] [int] NULL,
+	[tt_ngaytao] [date] NULL,
+	[tt_ngaysua] [date] NULL,
+	[tt_sodem] [int] NULL,
+	[tt_trangthai] [tinyint] NULL,
+	[tt_hot] [bit] NULL,
+ CONSTRAINT [PK_tintuc] PRIMARY KEY CLUSTERED 
+(
+	[tt_ma] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Comment] ADD  DEFAULT ((0)) FOR [trangthai]
+GO
+ALTER TABLE [dbo].[tintuc] ADD  CONSTRAINT [DF_tintuc_tt_ngaytao]  DEFAULT (getdate()) FOR [tt_ngaytao]
+GO
+ALTER TABLE [dbo].[tintuc] ADD  CONSTRAINT [DF_tintuc_tt_ngaysua]  DEFAULT (getdate()) FOR [tt_ngaysua]
+GO
+ALTER TABLE [dbo].[Comment]  WITH CHECK ADD  CONSTRAINT [fk_Cm_tk] FOREIGN KEY([ma_taikhoan])
+REFERENCES [dbo].[taikhoan] ([ma])
+GO
+ALTER TABLE [dbo].[Comment] CHECK CONSTRAINT [fk_Cm_tk]
+GO
+ALTER TABLE [dbo].[Comment]  WITH CHECK ADD  CONSTRAINT [fk_Cm_tt] FOREIGN KEY([ma_tin])
+REFERENCES [dbo].[tintuc] ([tt_ma])
+GO
+ALTER TABLE [dbo].[Comment] CHECK CONSTRAINT [fk_Cm_tt]
+GO
+ALTER TABLE [dbo].[PERMISSION]  WITH CHECK ADD  CONSTRAINT [fk_pr_lvl] FOREIGN KEY([levelID])
+REFERENCES [dbo].[LEVEL] ([levelID])
+GO
+ALTER TABLE [dbo].[PERMISSION] CHECK CONSTRAINT [fk_pr_lvl]
+GO
+ALTER TABLE [dbo].[product]  WITH CHECK ADD  CONSTRAINT [fk_pro_cate] FOREIGN KEY([TypeProd])
+REFERENCES [dbo].[ProdCategory] ([IDCate])
+GO
+ALTER TABLE [dbo].[product] CHECK CONSTRAINT [fk_pro_cate]
+GO
+USE [master]
+GO
+ALTER DATABASE [tintuconline] SET  READ_WRITE 
+GO
